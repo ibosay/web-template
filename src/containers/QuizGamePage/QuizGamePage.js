2506639134152c6d02c0b59a46aefb4823da24c6
ibosay <ibragimt992@gmail.com>
2026-09-20 14:40:55 +0200

    Resolve current Quiz Arena TSX entry

--- src/containers/QuizGamePage/QuizGamePage.js
+++ src/containers/QuizGamePage/QuizGamePage.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { isScrollingDisabled } from '../../ducks/ui.duck';
 import { LayoutSingleColumn, Page } from '../../components';
 
-import QuizArenaLiveApp from './QuizArenaLiveApp';
+import QuizArenaLiveApp from './QuizArenaLiveApp.tsx';
 import css from './QuizGamePage.module.css';
 
 export const QuizGamePageComponent = props => {
