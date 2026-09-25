8b4bd8fb4d520e7f1ce8a7003ecb02ee854206ff
ibosay <ibragimt992@gmail.com>
2026-09-25 10:43:02 +0200

    Export free TCGdex provider

--- wertscan/cardData/index.ts
+++ wertscan/cardData/index.ts
@@ -8,3 +8,6 @@ export * from './fx';
 export { InMemoryCardDataProvider, StaticFxRateProvider } from './testProvider';
 // Der Scrydex-Adapter wird bewusst NICHT hier exportiert, damit er nie versehentlich in ein
 // Client-Bundle gelangt. Serverseitig direkt importieren: './cardData/scrydexProvider'.
+
+// Kostenlos, ohne API-Key. Darf auch in serverseitigen Builds direkt importiert werden.
+export { TcgDexProvider, tcgdexLocalId } from './tcgdexProvider';
